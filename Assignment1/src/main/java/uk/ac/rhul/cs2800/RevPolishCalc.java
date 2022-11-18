dc4fd9bd8a93827b4ecd8b74ae86597dff2c97f2
Khalid <zkac429@live.rhul.ac.uk>
2022-11-18 15:55:37 +0000

    Added an evaluate method and started with 2 simple tests

--- Assignment1/src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
+++ Assignment1/src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
@@ -1,5 +1,11 @@
 package uk.ac.rhul.cs2800;
 
 public class RevPolishCalc {
-
+  private NumStack values = new NumStack();
+  
+  public float evaluate(String expression) {
+    while(expression != "")
+      return 2;
+    return 0;
+  }
 }
--- Assignment1/src/test/java/uk/ac/rhul/cs2800/RevPolishCalcTest.java
+++ Assignment1/src/test/java/uk/ac/rhul/cs2800/RevPolishCalcTest.java
@@ -1,13 +1,25 @@
 package uk.ac.rhul.cs2800;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RevPolishCalcTest {
+  RevPolishCalc calc;
+  
+  @BeforeEach
+  public void setup() {
+    calc = new RevPolishCalc();
+  }
 
   @Test
-  void testClass() {
-    RevPolishCalc calc = new RevPolishCalc();
+  void testEvaluate0() {
+    assertEquals(0, calc.evaluate(""), "Tests if an empty string returns a 0");
+  }
+  
+  @Test
+  void testEvaluateNumber() {
+    assertEquals(2, calc.evaluate("2"), "Tests if a number is returned if the string contains only character");
   }
 
 }
