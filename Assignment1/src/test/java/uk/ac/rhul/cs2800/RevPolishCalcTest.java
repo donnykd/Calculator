00dcae52c07d503a659086cf167e1944eaca682e
Khalid <zkac429@live.rhul.ac.uk>
2022-11-18 11:59:15 +0000

    Created revpolishcalc class

---
+++ Assignment1/src/test/java/uk/ac/rhul/cs2800/RevPolishCalcTest.java
@@ -0,0 +1,13 @@
+package uk.ac.rhul.cs2800;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class RevPolishCalcTest {
+
+  @Test
+  void testClass() {
+    RevPolishCalc calc = new RevPolishCalc();
+  }
+
+}
