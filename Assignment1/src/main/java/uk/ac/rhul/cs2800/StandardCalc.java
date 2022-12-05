8f9b1e67550bffb191f9fb69be565f24272b811c
Khalid <zkac429@live.rhul.ac.uk>
2022-12-05 01:31:14 +0000

    Refactored standardCalc
    Used switch cases, cleaned up some redundant code

--- Assignment1/src/main/java/uk/ac/rhul/cs2800/StandardCalc.java
+++ Assignment1/src/main/java/uk/ac/rhul/cs2800/StandardCalc.java
@@ -5,64 +5,64 @@ import java.util.Scanner;
 
 public class StandardCalc {
   private OpStack value = new OpStack();
-  RevPolishCalc rpcalc = new RevPolishCalc();
-  ArrayDeque<String> queue = new ArrayDeque<String>();
+  private RevPolishCalc rpcalc = new RevPolishCalc();
+  private ArrayDeque<String> queue = new ArrayDeque<String>();
 
   public float evaluate(String expression) throws Exception {
+    
     try (Scanner sc = new Scanner(expression)) {
       String next;
 
       while (sc.hasNext()) {
         next = sc.next();
 
-        if (Character.isDigit(next.charAt(0)))
-          queue.push(next + " ");
-
-        else if (next.equals(Symbol.PLUS.toString())) {
-          if (!(value.isEmpty())) {
-            if (value.top().equals(Symbol.TIMES) || value.top().equals(Symbol.DIVIDE)) {
-              String temp = value.pop().toString();
-              queue.push(temp + " ");
+        switch (next) {
+          
+          case "+":
+            if (!(value.isEmpty())) {
+              if (value.top().equals(Symbol.TIMES) || value.top().equals(Symbol.DIVIDE))
+                queue.push(value.pop().toString() + " ");
             }
-          }
-          value.push(Symbol.PLUS);
-        }
+            value.push(Symbol.PLUS);
+            break;
 
-        else if (next.equals(Symbol.MINUS.toString())) {
-          if (!(value.isEmpty())) {
-            if (value.top().equals(Symbol.TIMES) || value.top().equals(Symbol.DIVIDE)) {
-              String temp = value.pop().toString();
-              queue.push(temp + " ");
+          case "-":
+            if (!(value.isEmpty())) {
+              if (value.top().equals(Symbol.TIMES) || value.top().equals(Symbol.DIVIDE))
+                queue.push(value.pop().toString() + " ");
             }
-          }
-          value.push(Symbol.MINUS);
-        }
-        
-        else if (next.equals(Symbol.DIVIDE.toString()))
-          value.push(Symbol.DIVIDE);
+            value.push(Symbol.MINUS);
+            break;
 
-        else if (next.equals(Symbol.TIMES.toString()))
-          value.push(Symbol.TIMES);
-        
-        else if (next.equals(Symbol.LEFT_BRACKET.toString()))
-          value.push(Symbol.LEFT_BRACKET);
-        
-        else if (next.equals(Symbol.RIGHT_BRACKET.toString())) {
-          while(!(value.top().equals(Symbol.LEFT_BRACKET))) {
-            String temp = value.pop().toString();
-            queue.push(temp + " ");
-          }
+          case "/":
+            value.push(Symbol.DIVIDE);
+            break;
+
+          case "*":
+            value.push(Symbol.TIMES);
+            break;
+
+          case "(":
+            value.push(Symbol.LEFT_BRACKET);
+            break;
+
+          case ")":
+            while (!(value.top().equals(Symbol.LEFT_BRACKET)))
+              queue.push(value.pop().toString() + " ");
+            break;
+            
+          default:
+            queue.push(next + " ");
         }
       }
-      while (!(value.isEmpty())) {
-        String temp = value.pop().toString();
-        queue.push(temp + " ");
-      }
+      while (!(value.isEmpty()))
+        queue.push(value.pop().toString() + " ");
     }
+    
     StringBuilder sb = new StringBuilder();
-    while (!(queue.isEmpty())) {
+    
+    while (!(queue.isEmpty())) 
       sb.append(queue.removeLast());
-    }
 
     return rpcalc.evaluate(sb.toString());
   }
